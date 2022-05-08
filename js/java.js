e1aef65aa94013fe3ac2df8c9d65cb662e412506
BIDA21-047 <bida21-047@thuto.bac.ac.bw>
2022-05-08 17:03:47 +0200

    commit

--- js/java.js
+++ js/java.js
@@ -1,3 +1,10 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+function toggle(){
+    var trailer= document.querySelector(".trailer")
+    var iframe= document.querySelector("iframe")
+    trailer.classList.toggle("active");
+     }
+
+   
